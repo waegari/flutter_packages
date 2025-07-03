4e6f15b6aa41e95d22ab7b769278d981f3b8ace2
waegari <inductionhypothesis@gmail.com>
2025-07-03 13:50:30 +0900

    youtube link parse

--- packages/video_player/video_player/lib/video_player.dart
+++ packages/video_player/video_player/lib/video_player.dart
@@ -9,6 +9,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:video_player_platform_interface/video_player_platform_interface.dart';
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 import 'src/closed_caption_file.dart';
 
@@ -285,6 +286,8 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
        dataSourceType = DataSourceType.asset,
        formatHint = null,
        httpHeaders = const <String, String>{},
+       youtubeVideoQuality = null,
+       isYTLink = null,
        super(const VideoPlayerValue(duration: Duration.zero));
 
   /// Constructs a [VideoPlayerController] playing a network video.
@@ -308,6 +311,8 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
     this.videoPlayerOptions,
     this.httpHeaders = const <String, String>{},
     this.viewType = VideoViewType.textureView,
+    this.youtubeVideoQuality,
+    this.isYTLink,
   }) : _closedCaptionFileFuture = closedCaptionFile,
        dataSourceType = DataSourceType.network,
        package = null,
@@ -329,6 +334,8 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
     this.videoPlayerOptions,
     this.httpHeaders = const <String, String>{},
     this.viewType = VideoViewType.textureView,
+    youtubeVideoQuality = null,
+    isYTLink = null,
   }) : _closedCaptionFileFuture = closedCaptionFile,
        dataSource = url.toString(),
        dataSourceType = DataSourceType.network,
@@ -361,6 +368,8 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
     Future<ClosedCaptionFile>? closedCaptionFile,
     this.videoPlayerOptions,
     this.viewType = VideoViewType.textureView,
+    this.youtubeVideoQuality,
+    this.isYTLink,
   }) : assert(
          defaultTargetPlatform == TargetPlatform.android,
          'VideoPlayerController.contentUri is only supported on Android.',
@@ -376,6 +385,12 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   /// The URI to the video file. This will be in different formats depending on
   /// the [DataSourceType] of the original video.
   final String dataSource;
+
+  /// youtube video quality, default: VideoQuality.medium360
+  final VideoQuality? youtubeVideoQuality;
+
+  /// the URI to the youtube or not.
+  final bool? isYTLink;
 
   /// HTTP headers used for the request to the [dataSource].
   /// Only for [VideoPlayerController.network].
@@ -419,6 +434,36 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   @visibleForTesting
   int get playerId => _playerId;
 
+  /// To Get VideoId from Url
+  static String? _getIdFromUrl(String url, [bool trimWhitespaces = true]) {
+    List<RegExp> _regexps = [
+      RegExp(
+        r'^https:\/\/(?:www\.|m\.)?youtube\.com\/watch\?v=([_\-a-zA-Z0-9]{11}).*$',
+      ),
+      RegExp(
+        r'^https:\/\/(?:www\.|m\.)?youtube(?:-nocookie)?\.com\/embed\/([_\-a-zA-Z0-9]{11}).*$',
+      ),
+      RegExp(r'^https:\/\/youtu\.be\/([_\-a-zA-Z0-9]{11}).*$'),
+    ];
+
+    if (url == null || url.isEmpty) {
+      return null;
+    }
+
+    if (trimWhitespaces) {
+      url = url.trim();
+    }
+
+    for (RegExp exp in _regexps) {
+      final Match? match = exp.firstMatch(url);
+      if (match != null && match.groupCount >= 1) {
+        return match.group(1);
+      }
+    }
+
+    return null;
+  }
+
   /// Attempts to open the given [dataSource] and load metadata about the video.
   Future<void> initialize() async {
     final bool allowBackgroundPlayback =
@@ -428,6 +473,36 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
     }
     _lifeCycleObserver?.initialize();
     _creatingCompleter = Completer<void>();
+
+    final VideoQuality quality = youtubeVideoQuality ?? VideoQuality.medium360;
+
+    String finalYoutubeUrl = dataSource;
+    if (_getIdFromUrl(dataSource) != null && (isYTLink ?? false)) {
+      try {
+        Map<String, String> videoUrls = Map();
+        final String? _videoId = _getIdFromUrl(dataSource);
+        String _fetchUrl = "";
+
+        YoutubeExplode yt = YoutubeExplode();
+
+        final StreamManifest manifest = await yt.videos.streamsClient
+            .getManifest(_videoId);
+
+        Uri? videoUri;
+        for (final MuxedStreamInfo m in manifest.muxed) {
+          if (quality == m.videoQuality) {
+            videoUri = m.url;
+          }
+        }
+        if (videoUri == null) {
+          finalYoutubeUrl = manifest.muxed.first.url.toString();
+        } else {
+          finalYoutubeUrl = videoUri.toString();
+        }
+      } catch (err) {
+        throw 'getIdFromUrl error: $err';
+      }
+    }
 
     late DataSource dataSourceDescription;
     switch (dataSourceType) {
@@ -440,7 +515,7 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
       case DataSourceType.network:
         dataSourceDescription = DataSource(
           sourceType: DataSourceType.network,
-          uri: dataSource,
+          uri: finalYoutubeUrl,
           formatHint: formatHint,
           httpHeaders: httpHeaders,
         );
@@ -453,7 +528,7 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
       case DataSourceType.contentUri:
         dataSourceDescription = DataSource(
           sourceType: DataSourceType.contentUri,
-          uri: dataSource,
+          uri: finalYoutubeUrl,
         );
     }
 
@@ -836,6 +911,7 @@ class _VideoAppLifeCycleObserver extends Object with WidgetsBindingObserver {
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     if (state == AppLifecycleState.paused) {
+      print('AppLifecycleState.paused');
       if (!(_controller.value.size == Size(0.0, 0.0))) {
         _wasPlayingBeforePause = _controller.value.isPlaying;
         _controller.pause();
