f8323412da787ccad5df83f22358b0371a592f13
waegari <inductionhypothesis@gmail.com>
2025-07-03 13:30:19 +0900

    duration 0 허용

--- packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation/FVPVideoPlayer.m
+++ packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation/FVPVideoPlayer.m
@@ -389,9 +389,9 @@ NS_INLINE CGFloat radiansToDegrees(CGFloat radians) {
     }
     // The player may be initialized but still needs to determine the duration.
     int64_t duration = [self duration];
-    if (duration == 0) {
-      return;
-    }
+    // if (duration == 0) {
+    //   return;
+    // }
 
     _isInitialized = YES;
     [self updatePlayingState];
